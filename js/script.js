8ec8565dfc81755cbc2a384ba51d09d1099e5afb
Kenny Le <99355017+Kenny-Le-281@users.noreply.github.com>
2022-04-24 17:23:29 +0000

    changes to script.js

--- js/script.js
+++ js/script.js
@@ -11,9 +11,49 @@ if (navigator.serviceWorker) {
   })
 }
 
+;("use strict")
+
 /**
- * This function displays an alert.
+ * This function checks if you guessed the right number.
  */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+function total() {
+  // input
+  const taxRate = 0.13
+  const large = parseInt(document.getElementById("size-large").value)
+  const extraLarge = parseInt(document.getElementById("size-extra-large").value)
+  const oneTopping = parseFloat(document.getElementById("first-topping").value)
+  const twoTopping = parseFloat(document.getElementById("two-toppings").value)
+  const threeTopping = parseFloat(document.getElementById("three-toppings").value)
+  const fourTopping = parseFloat(document.getElementById("four-toppings").value)
+  const pizzaSize = parseFloat(document.getElementById("pizza-size").value)
+  const pizzaTopping = parseFloat(document.getElementById("pizza-topping").value)
+
+  // process and output
+  if (large.Checked == True) {
+    pizzaSize = 6.00
+  }
+
+  if (extraLarge.Checked == True) {
+    pizzaSize = 10.00
+  }
+
+  if (oneTopping.Checked == True) {
+    pizzaTopping = 1.00
+  }
+
+  if (twoTopping.Checked == True) {
+    pizzaTopping = 1.75
+  }
+
+  if (threeTopping.Checked == True) {
+    pizzaTopping = 2.50
+  }
+
+  if (fourTopping.Checked == True) {
+    pizzaTopping = 3.35
+  }
+
+  const totalCost = (pizzaSize + pizzaToping) * (1.00 - taxRate)
+
+  document.getElementById('total-cost').innerHTML = 'The total cost will be:' + totalCost.toFixed(2)
 }
